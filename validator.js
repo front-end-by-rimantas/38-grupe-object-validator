58d5a57173f4349340b39d0b33f5b886df9709bd
Rimantas <belauzas@yahoo.com>
2022-06-27 20:42:36 +0300

    Validator solution

--- validator.js
+++ validator.js
@@ -2,10 +2,12 @@
  * Duomenu objekto validatorius, kuris tikrina, ar duomenu objekte yra tik leistini raktazodziai.
  * 
  * Objekte gali buti ir papildomu neprivalomu, bet vis vien leistinu raktazodziu (optional keys)
- * @param {Object} rules Duomenu objektas
+ * @param {Object} obj Duomenu objektas
+ * @param {Object} rules Nustatymu objektas
  * @returns {[boolean, string]} Rezultatas, kur pirmasis parametras reiskia ar buvo rasta klaida, o antrasis - zinute (aprasanti klaida)
  */
 function validator(obj, rules) {
+    // tikriname duomenu tipus
     if (typeof obj !== 'object'
         || obj === null
         || Array.isArray(obj)) {
@@ -16,6 +18,42 @@ function validator(obj, rules) {
         || rules === null
         || Array.isArray(rules)) {
         return [true, 'Neduotas strukturos objektas'];
+    }
+
+    // uzisipildome trukstama informacija
+    if (!('required' in rules)) {
+        rules.required = [];
+    }
+    if (!('optional' in rules)) {
+        rules.optional = [];
+    }
+
+    const objKeys = Object.keys(obj);
+    const { required, optional } = rules;
+    const totalRulesKeys = [...required, ...optional];
+
+    // jei atejes objektas yra mazesnis uz privalomu lauku kieki
+    // tai jam akivaizdziai kazkokio privalomo lauko truksta
+    // net neidomu kokio, vis vien klaida
+    if (objKeys.length < required.length) {
+        return [true, 'Truksta privalomo key'];
+    }
+
+    // kai objektas neturi bent vieno privalomo lauko
+    // tai einam per privalomus ir ziurim, kurio lauko atejes
+    // duomenu objektas savyje neturi
+    for (const reqKey of required) {
+        if (!objKeys.includes(reqKey)) {
+            return [true, 'Truksta privalomo key'];
+        }
+    }
+
+    // jei atejes objektas savyje turi bent viena lauka
+    // kuris nera tarp leistinu lauku, tai klaida
+    for (const objKey of objKeys) {
+        if (!totalRulesKeys.includes(objKey)) {
+            return [true, 'Pateiktas netinkamas key'];
+        }
     }
 
     return [false, 'OK'];
--- validator.test.js
+++ validator.test.js
@@ -85,212 +85,286 @@ describe('Negatyvus variantai', () => {
         expect(msg).toBe('Truksta privalomo key');
     })
 
-    //     test('many required', () => {
-    //         const target = {
-    //             name: 'Petras',
-    //             isMarried: true,
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('many required', () => {
+        const target = {
+            name: 'Petras',
+            isMarried: true,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('single required, single optional: not present', () => {
-    //         const target = {};
-    //         const rules = {
-    //             required: ['name'],
-    //             optional: ['color'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('single required, single optional: not present', () => {
+        const target = {};
+        const rules = {
+            required: ['name'],
+            optional: ['color'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('many required, single optional: not present', () => {
-    //         const target = {
-    //             age: 99,
-    //             isMarried: true,
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //             optional: ['color'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('many required, single optional: not present', () => {
+        const target = {
+            age: 99,
+            isMarried: true,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('single required, single optional: present', () => {
-    //         const target = {
-    //             color: 'red',
-    //         };
-    //         const rules = {
-    //             required: ['name'],
-    //             optional: ['color'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('single required, single optional: present', () => {
+        const target = {
+            color: 'red',
+        };
+        const rules = {
+            required: ['name'],
+            optional: ['color'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('many required, single optional: present', () => {
-    //         const target = {
-    //             name: 'Petras',
-    //             age: 99,
-    //             color: 'red',
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //             optional: ['color'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('many required, single optional: present', () => {
+        const target = {
+            name: 'Petras',
+            age: 99,
+            color: 'red',
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('many required, many optional: not present', () => {
-    //         const target = {
-    //             age: 99,
-    //             isMarried: true,
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //             optional: ['color', 'pet', 'kids'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('many required, many optional: not present', () => {
+        const target = {
+            age: 99,
+            isMarried: true,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color', 'pet', 'kids'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('many required, many optional: some present', () => {
-    //         const target = {
-    //             name: 'Petras',
-    //             age: 99,
-    //             color: 'red',
-    //             pet: 'Rex',
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //             optional: ['color', 'pet', 'kids'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('many required, many optional: some present', () => {
+        const target = {
+            name: 'Petras',
+            age: 99,
+            color: 'red',
+            pet: 'Rex',
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color', 'pet', 'kids'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('many required, many optional: all present', () => {
-    //         const target = {
-    //             age: 99,
-    //             isMarried: true,
-    //             color: 'red',
-    //             pet: 'Rex',
-    //             kids: 2,
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //             optional: ['color', 'pet', 'kids'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Truksta privalomo key');
-    //     })
+    test('many required, many optional: all present', () => {
+        const target = {
+            age: 99,
+            isMarried: true,
+            color: 'red',
+            pet: 'Rex',
+            kids: 2,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color', 'pet', 'kids'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('no required, many optional: no present', () => {
-    //         const target = {
-    //             isAdmin: true,
-    //         };
-    //         const rules = {
-    //             optional: ['color', 'pet', 'kids'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Pateiktas netinkamas key');
-    //     })
+    test('no required, many optional: no present', () => {
+        const target = {
+            isAdmin: true,
+        };
+        const rules = {
+            optional: ['color', 'pet', 'kids'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
 
-    //     test('no required, many optional: some present', () => {
-    //         const target = {
-    //             isAdmin: true,
-    //             color: 'red',
-    //         };
-    //         const rules = {
-    //             optional: ['color', 'pet', 'kids'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Pateiktas netinkamas key');
-    //     })
+    test('no required, many optional: some present', () => {
+        const target = {
+            isAdmin: true,
+            color: 'red',
+        };
+        const rules = {
+            optional: ['color', 'pet', 'kids'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
 
-    //     test('no required, many optional: all present', () => {
-    //         const target = {
-    //             isAdmin: true,
-    //             color: 'red',
-    //             pet: 'Rex',
-    //             kids: 2,
-    //         };
-    //         const rules = {
-    //             optional: ['color', 'pet', 'kids'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Pateiktas netinkamas key');
-    //     })
+    test('no required, many optional: all present', () => {
+        const target = {
+            isAdmin: true,
+            color: 'red',
+            pet: 'Rex',
+            kids: 2,
+        };
+        const rules = {
+            optional: ['color', 'pet', 'kids'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
 
-    //     test('no required, no optional', () => {
-    //         const target = {
-    //             isAdmin: true,
-    //         };
-    //         const rules = {};
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Pateiktas netinkamas key');
-    //     })
+    test('no required, no optional', () => {
+        const target = {
+            isAdmin: true,
+        };
+        const rules = {};
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
 
-    //     test('all required, no optional, single extra', () => {
-    //         const target = {
-    //             age: 99,
-    //             isMarried: true,
-    //             isAdmin: true,
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Pateiktas netinkamas key');
-    //     })
+    test('all required, no optional, single extra', () => {
+        const target = {
+            age: 99,
+            isMarried: true,
+            isAdmin: true,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('all required, no optional, many extra', () => {
-    //         const target = {
-    //             age: 99,
-    //             isMarried: true,
-    //             isAdmin: true,
-    //             adminName: 'Hacker',
-    //             rules: 69420,
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Pateiktas netinkamas key');
-    //     })
+    test('all required, no optional, many extra', () => {
+        const target = {
+            age: 99,
+            isMarried: true,
+            isAdmin: true,
+            adminName: 'Hacker',
+            rules: 69420,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
 
-    //     test('many required, no optional, many extra', () => {
-    //         const target = {
-    //             isAdmin: true,
-    //             adminName: 'Hacker',
-    //             rules: 69420,
-    //         };
-    //         const rules = {
-    //             required: ['name', 'age', 'isMarried'],
-    //         };
-    //         const [err, msg] = validator(target, rules);
-    //         expect(err).toBe(true);
-    //         expect(msg).toBe('Pateiktas netinkamas key');
-    //     })
+    test('many required, no optional, many extra', () => {
+        const target = {
+            isAdmin: true,
+            adminName: 'Hacker',
+            rules: 69420,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Truksta privalomo key');
+    })
+
+    test('all required, no optional, many extra', () => {
+        const target = {
+            name: 'Petras',
+            age: 99,
+            isMarried: true,
+            isAdmin: true,
+            adminName: 'Hacker',
+            rules: 69420,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
+
+    test('all required, no optional, many extra', () => {
+        const target = {
+            name: 'Petras',
+            age: 99,
+            isMarried: true,
+            isAdmin: true,
+            adminName: 'Hacker',
+            rules: 69420,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color', 'pet'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
+
+    test('all required, single optional, many extra', () => {
+        const target = {
+            name: 'Petras',
+            age: 99,
+            isMarried: true,
+            color: 'red',
+            isAdmin: true,
+            adminName: 'Hacker',
+            rules: 69420,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color', 'pet'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
+
+    test('all required, all optional, many extra', () => {
+        const target = {
+            name: 'Petras',
+            age: 99,
+            isMarried: true,
+            color: 'red',
+            pet: 'dog',
+            isAdmin: true,
+            adminName: 'Hacker',
+            rules: 69420,
+        };
+        const rules = {
+            required: ['name', 'age', 'isMarried'],
+            optional: ['color', 'pet'],
+        };
+        const [err, msg] = validator(target, rules);
+        expect(err).toBe(true);
+        expect(msg).toBe('Pateiktas netinkamas key');
+    })
 })
 
 describe('Geri variantai', () => {
